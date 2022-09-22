d25a3b22a345111943396958ec0b08ad81ec4630
sagsshakya <sags.shakya@gmail.com>
2022-09-22 11:39:12 +0545

    Added config -> filename

--- process.py
+++ process.py
@@ -1,11 +1,23 @@
 import json
+import os
+
 from inputs import get_input
+from utils import load_config
+
+# Load Configuration file.
+config = load_config()
 
 # Prompt for input.
 day, absentees = get_input(verbose = False)
 
 # Access the database.
-df = json.load(open('result.json'))    
+save_dir = config['database_path']['root']
+routine_filename = config['database_path']['routine_db_path']
 
+routine_filename = os.path.join(save_dir, routine_filename)
+
+df = json.load(open(routine_filename))    
+
+print(df)
 #if __name__ == '__main__':
 #    print(main())
